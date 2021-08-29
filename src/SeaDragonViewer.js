cc738ba0bc9e44926bcb7d2176b7f703d76f4a18
aragon12 <sm95007@gmail.com>
2021-08-29 14:51:33 +0530

    sdviewer: overlay support for OSD
    used fabricjs overlay for OSD
    now we can draw basic shapes over image, like circle & rectangle

--- src/SeaDragonViewer.js
+++ src/SeaDragonViewer.js
@@ -1,11 +1,15 @@
-import OpenSeaDragon from "openseadragon";
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
+import {fabric, initFabricJSOverlay} from '@adamjarling/openseadragon-fabricjs-overlay';
+import {OpenSeadragon} from "use-open-seadragon";
+import {IconButton, Tooltip} from '@material-ui/core';
 import ZoomInIcon from '@material-ui/icons/ZoomIn';
 import ZoomOutIcon from '@material-ui/icons/ZoomOut';
 import FullScreenIcon from '@material-ui/icons/Fullscreen';
-import { IconButton, Tooltip} from '@material-ui/core';
-import './App.css';
 import YoutubeSearchedForIcon from "@material-ui/icons/YoutubeSearchedFor";
+import CheckBoxOutlineBlankIcon from "@material-ui/icons/CheckBoxOutlineBlank";
+import RadioButtonUncheckedIcon from "@material-ui/icons/RadioButtonUnchecked";
+import './App.css';
+
 
 const sdConf = {
   id: "openSeaDragon",
@@ -23,24 +27,59 @@ const sdConf = {
   fullPageButton: "fullscreen",
 }
 
+var rect = new fabric.Rect({
+  top: 200,
+  left: 200,
+  width: 200,
+  height: 200,
+  fill: 'transparent',
+  stroke:
+  'red',
+  strokeWidth: 5
+});
+
+var cir = new fabric.Circle({
+  top: 200,
+  left: 200,
+  radius: 100,
+  fill: 'transparent',
+  stroke:
+  'red',
+  strokeWidth: 5
+});
+
 const toolTipDelay = 700;
 
 function SeaDragonViewer(props) {
-  const [viewer, setViewer] = useState( null);
+  const [viewer, setViewer] = useState(null);
+  const [overlay, setOverlay] = useState(null);
+
+  initFabricJSOverlay(OpenSeadragon, fabric);
 
   useEffect(() => {
-    if (props.slide && viewer) {
+    if (props.slide && viewer && overlay) {
       viewer.open(props.slide.source);
+      overlay.fabricCanvas().clear();
     }
   }, [props.slide]);
 
   useEffect(() => {
     viewer && viewer.destroy();
-    setViewer(OpenSeaDragon(sdConf));
+    const v = OpenSeadragon(sdConf);
+    setViewer(v);
+    setOverlay(v.fabricjsOverlay({scale: 1}));
     return () => {
         viewer && viewer.destroy();
     };
   }, []);
+
+  const addRect = (e) => {
+    overlay.fabricCanvas().add(rect);
+  }
+
+  const addCircle = (e) => {
+    overlay.fabricCanvas().add(cir);
+  }
 
   return (
     <div>
@@ -69,6 +108,16 @@ function SeaDragonViewer(props) {
   <Tooltip title="Fullscreen" enterDelay={toolTipDelay}>
     <IconButton color="primary" id="fullscreen">
       <FullScreenIcon fontSize="large" />
+    </IconButton>
+  </Tooltip>
+  <Tooltip title="Add circle" enterDelay={toolTipDelay}>
+    <IconButton color="primary" onClick={addCircle}>
+      <RadioButtonUncheckedIcon fontSize="large" />
+    </IconButton>
+  </Tooltip>
+  <Tooltip title="Add rectangle" enterDelay={toolTipDelay}>
+    <IconButton color="primary" onClick={addRect}>
+      <CheckBoxOutlineBlankIcon fontSize="large" />
     </IconButton>
   </Tooltip>
   </div>
